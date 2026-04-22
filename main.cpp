c770952faba16fe4da2fc9d17ce5ae5dd2f7b3a8
Sviatoslav <sviatoslav6239@gmail.com>
2026-04-22 12:26:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-bool run = true;
-
 struct todo
 {
     int id;
@@ -21,23 +19,58 @@ int main()
     string title;
     string description;
 
-    cout << "enter title of your new todo:\n>>>";
-    cin >> title;
+    int option;
 
-    cout << "enter the description of your new todo:\n>>>";
-    cin >> description;
+    bool run = true;
 
-    new_todo.id = todos.size() + 1;
-    new_todo.title = title;
-    new_todo.description = description;
-
-    todos.push_back(new_todo);
-
-    for (size_t i = 0; i < todos.size(); i++)
+    while (run == true)
     {
-        cout << "id: " << todos[i].id << "\n"
-             << "title: " << todos[i].title << "\n"
-             << "description: " << todos[i].description << "\n";
+        cout << "What would you do today?\n"
+             << "1.List of the todos. "
+             << "2.Add new the todo. "
+             << "3.Delete the todo. "
+             << "4.Update the todo. "
+             << "5.Exit.\n>>>";
+        cin >> option;
+
+        if (option == 1)
+        {
+            for (size_t i = 0; i < todos.size(); i++)
+            {
+                cout << "id: " << todos[i].id << "\n"
+                     << "title: " << todos[i].title << "\n"
+                     << "description: " << todos[i].description << "\n";
+            }
+        }
+        else if (option == 2)
+        {
+            cout << "enter title of your new todo:\n>>>";
+            cin >> title;
+
+            cout << "enter the description of your new todo:\n>>>";
+            cin >> description;
+
+            cout << "You have created new todo: " << title << " with description: " << description << "\n";
+
+            new_todo.id = todos.size() + 1;
+            new_todo.title = title;
+            new_todo.description = description;
+
+            todos.push_back(new_todo);
+
+            continue;
+        }
+        else if (option == 3)
+        {
+        }
+        else if (option == 4)
+        {
+        }
+        else if (option == 5)
+        {
+            cout << "Exiting...";
+            break;
+        }
     }
 
     return 0;
